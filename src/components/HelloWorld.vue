e4b4babb0a755327544a0e17a632a118dbc27425
Myeonghak <dhaudgkr@gmail.com>
2019-07-15 00:23:15 +0900

    0.2 add bootstrap-vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div>
+        <b-button>Button</b-button>
+        <b-button variant="danger">Button</b-button>
+        <b-button variant="success">Button</b-button>
+        <b-button variant="outline-primary">Button</b-button>
+    </div>
   </div>
 </template>
 
