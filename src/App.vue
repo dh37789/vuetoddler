75e7c8def8f2f746732aec687f9156b9335cf57d
MyeonghakOh <dhaudgkr@gmail.com>
2019-07-15 17:19:00 +0900

    add Argon Template

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,15 @@
 <template>
-  <div id="app">
-    <div id="header">
-      <main-navbar/>
+    <div id="app">
+        <router-view name="header"></router-view>
+        <router-view name="footer"></router-view>
     </div>
-    
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="안녕하세요 오명학의 VuePage입니다."/>
-    
-    <div id="footer">
-      <main-footer/>
-    </div>
-  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import navbar from './components/navbar.vue'
-import footer from './components/footer.vue'
+import { FadeTransition } from "vue2-transitions";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld,
-    'main-navbar': navbar,
-    'main-footer': footer
+    FadeTransition
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
